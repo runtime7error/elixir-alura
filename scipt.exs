c29b1919cb54547cd1ca014c3dae6346fc406cbf
Lucas Henrique <validacao01.inobag@gmail.com>
2022-05-05 14:57:29 -0300

    Uso do retorno das funções como parametro;

--- scipt.exs
+++ scipt.exs
@@ -1,5 +1,14 @@
 defmodule MeuModulo do
+    import IO, only: [puts: 1]
+    import Kernel, except: [inspect: 1]
+
     def hello_world do
-        IO.puts("Hello World !")
+        inspect("Hello World !")
+    end
+
+    def inspect(parametro) do
+        puts("Começando a inspeção...")
+        puts(parametro)
+        puts("Inspeção terminada!")
     end
 end
